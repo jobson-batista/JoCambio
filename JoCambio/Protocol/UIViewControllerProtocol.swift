c847f5aa86b98aafcd744740ffcfff8e10359d74
Jobson <eniac@free.local>
2025-05-12 22:09:16 -0300

    LauchScreen implemented with ImageView

---
+++ JoCambio/Protocol/UIViewControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController.swift
+//  JoCambio
+//
+//  Created by Jobson on 12/05/25.
+//
+
+protocol UIViewControllerProtocol {
+    
+    func setupConstraints()
+    func setupHierarchy()
+}
